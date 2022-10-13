5d844e5d56a66018bb66aa660553fc315681dcea
Mohamed Maskoul <maskoul.mohammed@gmail.com>
2022-10-13 19:01:59 +0100

    Add header and nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home/home.component';
+import { HeaderComponent } from './header/header/header.component';
+import { NavComponent } from './nav/nav/nav.component';
+import { HeroSectionComponent } from './hero-section/hero-section/hero-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    NavComponent,
+    HeroSectionComponent
   ],
   imports: [
     BrowserModule,
