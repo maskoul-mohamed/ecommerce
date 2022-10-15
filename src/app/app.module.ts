0e36e51b80a81ef5d5559a4747ebd68d716d301a
Mohamed Maskoul <maskoul.mohammed@gmail.com>
2022-10-15 11:42:38 +0100

    Add leatest product section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HomeComponent } from './component/home/home.component';
 import { HeaderComponent } from './component/header/header.component';
 import { NavComponent } from './component/nav/nav.component';
 import { HeroSectionComponent } from './component/hero-section/hero-section.component';
+import { LeatestProductsComponent } from './component/leatest-products/leatest-products.component';
+import { ShopLiftPageComponent } from './component/shop-lift-page/shop-lift-page.component';
+import { PageHeaderComponent } from './component/page-header/page-header.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +18,9 @@ import { HeroSectionComponent } from './component/hero-section/hero-section.comp
     HeaderComponent,
     NavComponent,
     HeroSectionComponent,
+    LeatestProductsComponent,
+    ShopLiftPageComponent,
+    PageHeaderComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/leatest-products/leatest-products.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-leatest-products',
+  templateUrl: './leatest-products.component.html',
+  styleUrls: ['./leatest-products.component.css']
+})
+export class LeatestProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
