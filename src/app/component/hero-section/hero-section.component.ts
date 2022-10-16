545fa9716d72a0165fc4ea16d80df530dba17f11
Mohamed Maskoul <maskoul.mohammed@gmail.com>
2022-10-17 00:45:34 +0100

    Add slider hero functionality

--- src/app/component/hero-section/hero-section.component.ts
+++ src/app/component/hero-section/hero-section.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs'
+import { Products } from 'src/app/interface/products.interface';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-hero-section',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroSectionComponent implements OnInit {
 
-  constructor() { }
+  sliderProducts : Products | any = [];
+  randomNumbers : number[] = [];
+  sliderProductsSubscription : Subscription | any;
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.random(3);
+    this.getProducts()
   }
 
+  getProducts(){
+    this.sliderProductsSubscription = this.productService.getLimitProducts(10).subscribe(
+      (data) => {
+        for(let i = 0; i< this.randomNumbers.length; i++){
+          this.sliderProducts.push(data[this.randomNumbers[i]])
+        }
+      }
+    )
+  }
+
+  
+
+  random(tries: number){
+    for(let i = 0; i< tries; i++){
+    
+      this.randomNumbers.push(Math.floor(Math.random() * 10)); 
+    }
+  }
 }
